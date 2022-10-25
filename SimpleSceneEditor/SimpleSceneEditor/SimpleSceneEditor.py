aae29a62716e069599903d0ddd809881cbd637fc
bcagan <30479930+bcagan@users.noreply.github.com>
2022-10-25 10:55:53 +0900

    Created Basic List Object

--- SimpleSceneEditor/SimpleSceneEditor/SimpleSceneEditor.py
+++ SimpleSceneEditor/SimpleSceneEditor/SimpleSceneEditor.py
@@ -1,3 +1,4 @@
+from pickle import OBJ
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
@@ -7,13 +8,15 @@ from PySide6.QtCore import Slot
 
 class Object3D:
     def __init__(self):
-        color = [122,122,122] #All objects default to gray
-        translation = [0.0,0.0,0.0]
-        indicator = 0 #0 = not any object
+        self.name = ""
+        self.color = [122,122,122] #All objects default to gray
+        self.translation = [0.0,0.0,0.0]
+        self.indicator = 0 #0 = not any object
         #Rotation defined once I figure out how to implement quaternions
 
     def delete(self, list):
         #Take list, remove object from list
+        list.deleteObject(self)
         list.update()
         
     def editprompt(self,list):
@@ -21,22 +24,57 @@ class Object3D:
         pass
 
     def edit(self,list):
-        self.delete(list)
-        #modify list
+        list.modifyObject(self)
         list.update()
 
 class Cube(Object3D):
     def __init__(self):
         super().__init__()
-        scale = [1.0,1.0,1.0]
-        indicator = 1 #1 = cube
+        self.scale = [1.0,1.0,1.0]
+        self.indicator = 1 #1 = cube
+        self.name = "cube" #will be changed in add to dict
 
 class Sphere(Object3D):
     
     def __init__(self):
         super().__init__()
-        radius = 1.0
-        indicator = 2 #2 = sphere
+        self.radius = 1.0
+        self.indicator = 2 #2 = sphere
+        self.name = "sphere" #will be changed in add to dict
+
+class ObjectList():
+
+    def __init__(self):
+        self.objectDict = {}
+        self.cubeNum = 0
+        self.sphereNum = 0
+
+    def addObject(self, object):
+        if object.indicator == 0:
+            pass
+        elif object.indicator == 1: #cube
+            newName = "cube" + str(self.cubeNum)
+            self.cubeNum += 1
+            object.name = newName
+            self.objectDict[object.name] = object
+        else:
+            newName = "sphere" + str(self.sphereNum)
+            self.sphereNum += 1
+            object.name = newName
+
+    def deleteObject(self, object):
+        if object.indicator == 0:
+            pass
+        self.objectDict.pop(object.name)
+
+
+    def modifyObject(self, object):
+        if object.indicator == 0:
+            pass
+        self.objectDict[object.name]
+
+    def update(self):
+        pass
 
 class MyWidget (QtWidgets.QWidget):
     def __init__(self):
